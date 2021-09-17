b5ad8e2a16019169f399c3f1322ff68b1e8bbd82
tmitrevski <tmitrev1@gmail.com>
2021-09-16 23:21:05 -0400

    Worked on math, still more to do

--- queries.py
+++ queries.py
@@ -5,7 +5,7 @@ import pandas as pd
 # %%
 query = """query pools {
   pools(
-    block: {number: 1312667}
+    block: {number: 1880724}
     orderBy: totalValueLockedUSD
     orderDirection: desc
     subgraphError: allow
@@ -16,35 +16,57 @@ query = """query pools {
     sqrtPrice
     tick
     token0 {
-      id
       symbol
-      name
       decimals
-      derivedETH
-      __typename
     }
     token1 {
-      id
       symbol
-      name
       decimals
-      derivedETH
-      __typename
     }
+    volumeToken0
     token0Price
+    volumeToken1
     token1Price
     volumeUSD
     txCount
     totalValueLockedToken0
     totalValueLockedToken1
     totalValueLockedUSD
-    __typename
   }
 }
 """
 # %%
 url = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-optimism-dev'
 r = requests.post(url, json={'query': query})
-print(r.status_code)
-print(r.text)
+# %%
+data = r.json()
+# %%
+df = pd.DataFrame(data["data"]["pools"])
+df.head()
+# %%
+df['token0_decimals'] = [d.get('decimals') for d in df.token0]
+df['token0_name'] = [d.get('symbol') for d in df.token0]
+df.drop('token0', inplace=True, axis = 1)
+df['token1_decimals'] = [d.get('decimals') for d in df.token1]
+df['token1_name'] = [d.get('symbol') for d in df.token1]
+df.drop('token1', inplace=True, axis = 1)
+df.head()
+# %%
+uni_df = df.loc[((df['token0_name'] == 'ETH') & (df['token1_name'] == 'UNI'))]
+uni_df.head()
+# %%
+sqrtprice = ((914809224745660573156617212212)) / 2**(96)
+price = sqrtprice ** 2
+price
+# %%
+liq = float(uni_df['liquidity'][4]) * (10 ** (-1 * 18))
+liq
+# %%
+x = liq / sqrtprice
+y = liq * sqrtprice
+print(x)
+print(y)
+# %%
+yoverx = 54554.70361220090734665 / 392.773533017499984355
+yoverx
 # %%
